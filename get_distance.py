2f710241c2fb876333a499b5c94ec9c0a6784080
anonymous0111118 <141200895+anonymous0111118@users.noreply.github.com>
2023-08-03 20:32:37 +0800

    Update get_distance.py

--- get_distance.py
+++ get_distance.py
@@ -155,9 +155,9 @@ def get_distance(args, model, tokenizer, eval_when_training=False):
 
 
 def main():
-    with open(ROOT_PATH  + '/' + TYPE + '-bugs.txt', 'r') as f:
-        for eachLine in f:
-            bug.append(eachLine.strip())
+
+
+  
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--do_train", default=0, type=int)
